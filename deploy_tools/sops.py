007a5e4fe583e8e4360a8fea9cc6a29739b39612
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-07-04 16:16:18 +0100

    sops.py: Remove use of sops-wrapper

--- deploy_tools/sops.py
+++ deploy_tools/sops.py
@@ -11,17 +11,64 @@ import os
 from pathlib import Path
 import sys
 
+from hashlib import sha1
+import json
+
 from docopt import docopt
-import sh
+from sh import aws, sops
 
 
+AWS_PROFILE = "sops"
+CACHE_DIR = Path.home() / ".aws" / "cli" / "cache"
 DM_CREDENTIALS_REPO = Path(os.environ["DM_CREDENTIALS_REPO"])
-sops_wrapper = sh(_err=sys.stderr, _in=sys.stdin).sh.bake(DM_CREDENTIALS_REPO / "sops-wrapper")
 
 
-def pre_auth():
-    """Ask for MFA token"""
-    sops_wrapper("-v")
+def auth():
+    """Get credentials for communicating with AWS as environment variables"""
+    # We want to subsume the role of sops-wrapper and aws-auth
+    aws_profile = os.getenv("AWS_PROFILE", AWS_PROFILE)
+
+    def aws_configure_get(key, profile=aws_profile):
+        return aws.configure.get(key, profile=profile).strip()
+
+    aws.sts("get-caller-identity", _err=sys.stderr, _in=sys.stdin, _tty_in=True)
+    source_profile = aws_configure_get("source_profile")
+    role_arn = aws_configure_get("role_arn")
+    mfa_serial = aws_configure_get("mfa_serial")
+
+    def get_cache_file(role_arn, mfa_serial):
+        key = {"RoleArn": role_arn, "SerialNumber": mfa_serial}
+        plaintext = str(key).replace("'", '"').encode()
+        digest = sha1(plaintext).hexdigest()
+        return CACHE_DIR / f"{digest}.json"
+
+    cache_file = get_cache_file(role_arn, mfa_serial)
+    credentials = json.loads(cache_file.read_bytes())["Credentials"]
+
+    environ = {
+        "AWS_PROFILE": aws_profile,
+        "AWS_DEFAULT_REGION": aws_configure_get("region", profile=source_profile),
+        "AWS_ACCESS_KEY_ID": credentials["AccessKeyId"],
+        "AWS_SECRET_ACCESS_KEY": credentials["SecretAccessKey"],
+        "AWS_SECURITY_TOKEN": credentials["SessionToken"],
+        "AWS_SESSION_TOKEN": credentials["SessionToken"],
+    }
+
+    return environ
+
+
+def find_secrets_files(stage):
+    """Return the path to the secrets file for the stage
+
+    :param str stage: The stage to get secrets for, i.e. preview, staging, production
+    :return: The path to the secrets file for the specified stage
+    :rtype: Path
+    :raises ValueError: if the stage is not valid
+    """
+    fpath = DM_CREDENTIALS_REPO / "vars" / f"{stage}.yaml"
+    if not fpath.is_file():
+        raise ValueError(f"Expected a valid stage, but '{fpath}' does not exist")
+    return fpath
 
 
 def decrypt(fpath):
@@ -29,42 +76,28 @@ def decrypt(fpath):
 
     :param Path fpath: The file path
     :return: The decrypted contents
-    :rtype: str
+    :rtype: dict or str
     :raises ValueError: if the file does not exist
     """
     if not fpath.is_file():
-        raise ValueError(f"Expected a path to an existing file, but '{fpath}' is not a file or does not exist")
-    return sops_wrapper("-d", fpath)
-
-
-def get_secrets_for_stage(stage, decrypt=True):
-    """Return the secrets for stage, optionally decrypted
-
-    :param str stage: The stage to get secrets for, i.e. preview, staging, production
-    :param bool decrypt: Whether to decrypt or not
-    :return: The contents of the secrets file for the specified stage
-    :rtype: str
-    :raises ValueError: if the stage is not valid
-    """
-    fpath = DM_CREDENTIALS_REPO / "vars" / f"{stage}.yaml"
-    if not fpath.is_file():
-        raise ValueError(f"Expected a valid stage, but '{fpath}' does not exist")
-    if decrypt:
-        return decrypt(fpath)
-    else:
-        return fpath.read_text(encoding="utf8")
+        raise ValueError(
+            f"Expected a path to an existing file, but '{fpath}' is not a file or does not exist"
+        )
+    contents = sops("-d", fpath, _env=auth())
+    if str(fpath).endswith(".yaml"):
+        return yaml.safe_loads(contents)
+    return contents
 
 
 def main(argv=None):
     args = docopt(__doc__, argv=argv)
+    auth()
     if args["-v"]:
         logging.basicConfig(level=logging.INFO)
     if args["--stage"]:
         print(get_secrets_for_stage(args["--stage"]))
     elif args["<file>"]:
         print(decrypt(Path(args["<file>"])))
-    else:
-        pre_auth()
 
 
 if __name__ == "__main__":
