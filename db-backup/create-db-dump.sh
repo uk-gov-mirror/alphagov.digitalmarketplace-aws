bf19842636286e9e641790e2e2b1a2a90bd31532
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-06-22 17:31:51 +0100

    Update create-db-dump.sh

--- db-backup/create-db-dump.sh
+++ db-backup/create-db-dump.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-set +x  # Don't log credentials
-DB_URI=$(echo $VCAP_SERVICES | jq -r '.postgres[0].credentials.uri')
-set -x  # Restore logging
+# Don't log credentials
+DB_URI=$(echo $VCAP_SERVICES | jq -r '.postgres[0].credentials.uri') &>/dev/null
+
 echo -n "${PUBKEY}" > /app/public.key
 gpg2 --import /app/public.key
 pg_dump "${DB_URI}" --no-acl --no-owner --clean --if-exists | gzip | \
