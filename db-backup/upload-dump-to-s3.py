aab6e0aa93644d2ec3c72477b0df0afd90e44b02
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-12-04 09:07:43 +0000

    upload using curl

--- db-backup/upload-dump-to-s3.py
+++ db-backup/upload-dump-to-s3.py
@@ -5,30 +5,23 @@ import sys
 import json
 import requests
 from requests.exceptions import HTTPError
+import subprocess
 
 
 def upload_dump_to_s3():
     s3_post_url_data = json.loads(os.environ['S3_POST_URL_DATA'])
-    print("IN HERE")
-    print(s3_post_url_data)
     dump_file = "/app/{}".format(os.environ['DUMP_FILE_NAME'])
-
     url = s3_post_url_data['url']
     fields = s3_post_url_data['fields']
-    files = {"file": open(dump_file, 'rb')}
-
-    response = requests.post(url, data=fields, files=files)
-
-    try:
-        response.raise_for_status()
-    except HTTPError as e:
-        print("Error uploading {} to {}: {}".format(dump_file, url, e.args[0]))
-        sys.exit(1)
-    except Exception as e:
-        print("Error uploading: {}".format(e))
-        sys.exit(2)
-    else:
-        print('Successfully uploaded {} to {}'.format(dump_file, url))
+    curl_args = ['curl']
+    for k,v in fields:
+        curl_args.append('-F')
+        curl_args.append(f"${k}=${v}")
+    curl_args.append('-F')
+    curl_args.append(f"@file=${dump_file}")
+    curl_args.append(url)
+    subprocess.run(curl_args)
+    print('Successfully uploaded {} to {}'.format(dump_file, url))
 
 
 if __name__ == "__main__":
