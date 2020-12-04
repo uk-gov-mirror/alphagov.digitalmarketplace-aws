3e9716ae80f7460446359fb0c7dd540991a17ff1
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-12-04 15:26:04 +0000

    fixup

--- db-backup/upload-dump-to-s3.py
+++ db-backup/upload-dump-to-s3.py
@@ -13,7 +13,7 @@ def upload_dump_to_s3():
     dump_file = "/app/{}".format(os.environ['DUMP_FILE_NAME'])
     url = s3_post_url_data['url']
     fields = s3_post_url_data['fields']
-    curl_args = ['curl', '-F' f"@file=${dump_file}"]
+    curl_args = ['curl', '-F' f"@file={dump_file}"]
     for k,v in fields.items():
         curl_args.append('-F')
         curl_args.append(f"{k}={v}")
