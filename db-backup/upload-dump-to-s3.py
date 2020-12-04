e824d43d93afaa09481aa9733b9d6eda918a9632
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-12-04 15:49:58 +0000

    add error handling

--- db-backup/upload-dump-to-s3.py
+++ db-backup/upload-dump-to-s3.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
 import os
-import sys
 import json
-import requests
-from requests.exceptions import HTTPError
 import subprocess
 
 
@@ -13,12 +10,16 @@ def upload_dump_to_s3():
     dump_file = "/app/{}".format(os.environ['DUMP_FILE_NAME'])
     url = s3_post_url_data['url']
     fields = s3_post_url_data['fields']
-    curl_args = ['curl', '-F' f"@file={dump_file}"]
+    curl_args = ['curl', '-F', f"@file={dump_file}"]
     for k,v in fields.items():
         curl_args.append('-F')
         curl_args.append(f"{k}={v}")
     curl_args.append(url)
-    subprocess.run(curl_args)
+    try:
+        subprocess.check_output(curl_args)
+    except subprocess.CalledProcessError as e:
+        Exception(f"Upload failed with error: " + e.output)
+    
     print('Successfully uploaded {} to {}'.format(dump_file, url))
 
 
